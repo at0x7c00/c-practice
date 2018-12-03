a0a50a5d6a50b7bc6fcd7f24f28314b2560196a4
huqiao <zoozle@qq.com>
2018-12-03 10:57:08 +0800

    c89 vs c99

---
+++ modern-approach/chapter4/practice/p402.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void){
+  int i = 8,j = 5;
+  printf("%d / %d = %d\n",i,j,i/j);
+
+  i = -8;
+  printf("%d / %d = %d\n",i,j,i/j);
+
+  i = 8;
+  j = -5;
+  printf("%d / %d = %d\n",i,j,i/j);
+
+  i = -8;
+  printf("%d / %d = %d\n",i,j,i/j);
+
+  return 0;
+}
