d3af93ca9cf3d33623fa22af95fd3bfae85c1e67
huqiao <zoozle@qq.com>
2018-12-03 15:35:41 +0800

    chapter 6

--- modern-approach/chapter-6/fortest.c
+++ modern-approach/chapter-6/fortest.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
 int main(void){
-  for(int i = 0; i<100; i++){
+  printf("Hello,World");
+  int a = 120;
+  int i;
+  for(i = 0; i<100; i++){
     printf("%d\n",i);
   }
+  int x = 100;
+  x = x + 20;
+  printf("%d,%d",a,x);
   return 0;
 }
