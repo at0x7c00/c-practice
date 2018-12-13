f8791882823bc9661617422e5f4ff3c7a09ad54f
huqiao <zoozle@qq.com>
2018-12-13 16:00:43 +0800

    chapter7 fiddle

--- modern-approach/chapter-7/fiddle.c
+++ modern-approach/chapter-7/fiddle.c
@@ -1,17 +1,25 @@
 #include<stdio.h>
 #include<limits.h>
+int countInput();
 int main(void){
-  printf("%d\n",sizeof(short int));
-  printf("%d\n",sizeof(int));
-  printf("%d\n",sizeof(long int));
-
-
-
-  printf("max sort:%d\n",SHRT_MAX);
-  printf("max int:%d\n",INT_MAX);
-  printf("max long:%ld\n",LONG_MAX);
-
-
+/*
+printf("Enter a message:");
+int c = countInput();
+printf("Your message was %d character(s) long.",c);
+*/
+  typedef int Bool;
+  Bool x = 5;
+  printf("%d",x);
 
   return 0;
+}
+
+
+int countInput(){
+
+  int c = 0;
+  while((getchar()!='\n')){
+    c++;
+  }
+  return c;
 }
